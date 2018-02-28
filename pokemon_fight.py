12187d1f4fb8afcefde84be6efd390fa64d634fb
Lisandro <lisandrosaez@hotmail.com>
2018-02-28 00:26:05 -0300

    Pokemon fight

--- pokemon_fight.py
+++ pokemon_fight.py
@@ -3,29 +3,28 @@ oponent_choosen_pokemon = input("¿Contra que pokemon quieres luchar? (Squirtle/
 pikachu_life = 100
 if oponent_choosen_pokemon == "Squirtle":
     enemy_pokemon_life = 90
-if oponent_choosen_pokemon == "Charmander":
+    opponent_pokemon = "Squirtle"
+    enemy_pokemon_damage = 8
+elif oponent_choosen_pokemon == "Charmander":
     enemy_pokemon_life = 80
-if oponent_choosen_pokemon == "Bulbasaur":
+    opponent_pokemon = "Charmander"
+    enemy_pokemon_damage = 7
+elif oponent_choosen_pokemon == "Bulbasaur":
     enemy_pokemon_life = 100
+    opponent_pokemon = "Bulbasaur"
+    enemy_pokemon_damage = 10
 else:
     enemy_pokemon_life = 0
-
+    enemy_pokemon_damage = 0
 while pikachu_life > 0 and enemy_pokemon_life > 0:
     choosen_movement = input("¿Qué ataque vamos a usar? Chispazo/Bola Voltio:")
     if choosen_movement == "Chispazo":
         enemy_pokemon_life -= 10
-    if choosen_movement == "Bola Voltio":
+    elif choosen_movement == "Bola Voltio":
         enemy_pokemon_life -= 12
-    print("La vida del enemigo ahora es de {}".format(enemy_pokemon_life))
-    if oponent_choosen_pokemon == "Squirtle":
-        print("Squirtle te hace un ataque de 8 de daño")
-        pikachu_life -= 8
-    if oponent_choosen_pokemon == "Charmander":
-        print("Charmander te hace un ataque de 7 de daño")
-        pikachu_life -= 7
-    if oponent_choosen_pokemon == "Bulbasaur":
-        print("Bulbasaur te hace un ataque de 10 de daño")
-        pikachu_life -= 10
+    print("La vida del {} ahora es de {}".format(opponent_pokemon, enemy_pokemon_life))
+    print("{} te hace un ataque de {} de daño".format(opponent_pokemon,enemy_pokemon_damage))
+    pikachu_life -= enemy_pokemon_damage
     print("La vida de tu pokemon ahora es de {}".format(pikachu_life))
 if enemy_pokemon_life <= 0:
     print("¡Has ganado!")
