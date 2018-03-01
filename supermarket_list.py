24341e83e406af4ef6250dc5598e03a5e34af379
Lisandro <lisandrosaez@hotmail.com>
2018-03-01 13:16:13 -0300

    Lists and For

---
+++ supermarket_list.py
@@ -0,0 +1,10 @@
+my_list = ("Lechuga", "Tomate", "Helado", "Pan", "Pasta", "Olivas", "Atún", "Fanta")
+
+length_list = len(my_list)
+current_index = 0
+
+while current_index < length_list:
+    print("Tengo que comprar {}".format(my_list[current_index]))
+    current_index += 1
+
+print("Esta es la lista del super")
